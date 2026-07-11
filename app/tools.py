0169996d7e3f15c1819a6ed942541b42cf93590f
Seril Evanjaline <serilevanjaline122006@gmail.com>
2026-07-11 15:58:46 +0530

    feat : check_balance tool for agent to identify the balances of an employee

--- app/tools.py
+++ app/tools.py
@@ -6,7 +6,13 @@ _REQUESTS = json.loads(
     (config.DATA_DIR / "requests.json").read_text()
 )
 
+__BALANCES = json.loads(
+    (config.DATA_DIR / "balances.json").read_text()
+)
+
 _REQUESTS_BY_EMPLOYEE = {}
+
+_BALANCES_BY_EMPLOYEE = {}
 
 for request in _REQUESTS:
     employee_id = request["employee_id"]
@@ -17,6 +23,16 @@ for request in _REQUESTS:
     _REQUESTS_BY_EMPLOYEE[employee_id].append(request)
 
 
+
+for balance in __BALANCES:
+    employee_id = balance["employee_id"]
+    
+    if employee_id not in _BALANCES_BY_EMPLOYEE:
+        _BALANCES_BY_EMPLOYEE[employee_id] = []
+
+    _BALANCES_BY_EMPLOYEE[employee_id].append(balance)
+
+
 def list_leave_requests(employee_id: str,status: str | None = None,leave_type: str | None = None,) -> list[dict]:
     """
     Return all leave requests for an employee,
@@ -24,7 +40,7 @@ def list_leave_requests(employee_id: str,status: str | None = None,leave_type: s
     """
     employee_requests = _REQUESTS_BY_EMPLOYEE.get(employee_id, [])
     filtered_requests = []
-    
+
     for request in employee_requests:
         if status is not None and request["status"] != status:
             continue
@@ -33,3 +49,21 @@ def list_leave_requests(employee_id: str,status: str | None = None,leave_type: s
         filtered_requests.append(request)
 
     return filtered_requests
+
+
+
+
+def check_balance(employee_id: str,leave_type: str|None=None) -> list[dict]:
+    """
+    Return the leave balance for an employee and leave type,
+    or None if not found.
+    """
+    employee_balances  = _BALANCES_BY_EMPLOYEE.get(employee_id, [])
+    filtered_balances = []
+
+    for balance in employee_balances:
+        if leave_type is not None and balance["leave_type"] != leave_type:
+            continue
+        filtered_balances.append(balance)
+
+    return filtered_balances
