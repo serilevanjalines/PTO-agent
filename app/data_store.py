8ee4db727b4b0f6d621fe6fa43b0a8a5c9577194
Seril Evanjaline <serilevanjaline122006@gmail.com>
2026-07-18 09:53:01 +0530

    refrac : adding leave requests and balances loading

--- app/data_store.py
+++ app/data_store.py
@@ -12,3 +12,39 @@ for e in employees:
     key = e["id"]
     value = e
     _EMPLOYEES[key] = value
+
+
+_REQUESTS = json.loads(
+    (config.DATA_DIR / "requests.json").read_text()
+)
+
+_REQUESTS_BY_EMPLOYEE = {}
+
+
+for request in _REQUESTS:
+    employee_id = request["employee_id"]
+
+    if employee_id not in _REQUESTS_BY_EMPLOYEE:
+        _REQUESTS_BY_EMPLOYEE[employee_id] = []
+
+    _REQUESTS_BY_EMPLOYEE[employee_id].append(request)
+
+
+
+__BALANCES = json.loads(
+    (config.DATA_DIR / "balances.json").read_text()
+)
+
+
+_BALANCES_BY_EMPLOYEE = {}
+
+
+
+
+for balance in __BALANCES:
+    employee_id = balance["employee_id"]
+    
+    if employee_id not in _BALANCES_BY_EMPLOYEE:
+        _BALANCES_BY_EMPLOYEE[employee_id] = []
+
+    _BALANCES_BY_EMPLOYEE[employee_id].append(balance)
